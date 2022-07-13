cad4c6caae1a23ee6a021599bd6a7bcefc2c1af2
Clannad529 <595927593@qq.com>
2022-07-13 14:58:37 +0800

    .

--- Elder-front-end/src/components/Add_3.vue
+++ Elder-front-end/src/components/Add_3.vue
@@ -73,11 +73,13 @@ export default {
       can: true,
       fileList: [{name: 'food.jpeg', url: 'https://fuss10.elemecdn.com/3/63/4e7f3a15429bfda99bce42a18cdd1jpeg.jpeg?imageMogr2/thumbnail/360x360/format/webp/quality/100'}],
       formValidate: {
+        id:0,//新增
         name: '',
         id_card: '',
         birthday: '',
         gender: '',
         i_date: '',
+        o_date:'',//新增
         phone: '',
         desc: ''
       },
